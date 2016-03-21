463bcc98af6aea1549de53f1363240c3dc5d3aed
Timothy Chung <timchunght@gmail.com>
2016-03-21 01:52:52 -0400

    Started part-b and added function to import Q

---
+++ part-b/pca.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def import_file_as_Q(file_path):
+	input_file = open(file_path, 'r')
+	# first line is number of assets
+	asset_size =  int(input_file.readline().split()[1])
+	# create a n times n matrix
+	Q = np.ndarray(shape=(asset_size, asset_size))
+	for j in range(asset_size + 1):
+		current_line = input_file.readline().split()
+		if current_line[0] == "matrix":
+			pass
+		else:
+			for k in range(asset):
+				Q[j,k] = float(current_line[j])
+
+	return Q
+
+	
