3c1d8468089a99d24fdeac772eb258d6b6812ed6
Timothy Chung <timchunght@gmail.com>
2016-03-22 23:26:05 -0400

    Finished import function pca.py

--- part-b/pca.py
+++ part-b/pca.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time
 
 def import_Q_from_file(file_path):
 	input_file = open(file_path, 'r')
@@ -7,14 +7,19 @@ def import_Q_from_file(file_path):
 	asset_size =  int(input_file.readline().split()[1])
 	# create a n times n matrix
 	Q = np.ndarray(shape=(asset_size, asset_size))
-	for j in range(asset_size + 1):
+
+	input_file.readline()
+	for j in range(asset_size):
 		current_line = input_file.readline().split()
-		if current_line[0] == "matrix":
-			pass
-		else:
-			for k in range(asset):
-				Q[j,k] = float(current_line[j])
+		while len(current_line) == 0 or current_line[1] == "matrix":
+			current_line = input_file.readline()	
+			current_line = current_line.split()
+		print current_line
+		for k in range(asset_size):
+			Q[j,k] = float(current_line[j])
 
 	input_file.close()
 	return Q
 
+
+import_Q_from_file("/Users/timothychung/Documents/workspace/IEOR/hw4/part-a/rpower/data/russell-1000-Q.dat")
