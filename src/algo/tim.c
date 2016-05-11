0afb4476747f72f00a01b7145fabb00fc16fee0a
Timothy Chung <timchunght@gmail.com>
2016-05-10 23:39:40 -0400

    Implemented tim_optimize function

--- src/algo/tim.c
+++ src/algo/tim.c
@@ -55,13 +55,77 @@ int tim_resources_allocate(int N, int T, double alpha) {
 	printf("shares_count: %d, T (periods): %d\n", N, T);
 
 	double *probabilities = (double*)calloc((N+1)*(N+1), sizeof(double));
-	// double *optimal = (double*)calloc(T*(N+1), sizeof(double));
-	// double *path = (int*)calloc(T*(N+1), sizeof(int));
+	double *optimal = (double*)calloc(T*(N+1), sizeof(double));
+	double *path = (int*)calloc(T*(N+1), sizeof(int));
 	double *d = (double*)calloc(N+1, sizeof(double));
 	
 
 	calculate_alpha_log_priceshifts(d, N, alpha);
 	calculate_deterministic_probabilities(probabilities, N);
 	
+	tim_optimize(N, T, probabilities, optimal, path, d);
 	return 0;
 }
+
+
+// takes double arrs and execute optimization logic
+int tim_optimize(int N, int T, double *probabilities, double *optimal, double *path, double *d) {
+	
+	double expected_revenue;
+	double best_expected_revenue;
+	int best_index;
+
+	double profit;
+	double newprice;
+	int sell, k, kp;
+
+	int t = T-1;
+	
+	printf("t = %d\n", t); fflush(stdout);
+	best_expected_revenue = 0.0;
+	for (int n = 0; n <= N; n++) {
+		expected_revenue = 0.0;
+		k = n; 
+		for (kp = 0; kp <= k; kp++) {
+			expected_revenue += get_probability(probabilities, N, kp, k) * (double)kp;
+		}
+		expected_revenue *= d[k];
+		if (expected_revenue > best_expected_revenue) {
+			best_expected_revenue = expected_revenue;
+		}
+
+		F(optimal, N, t, n) = best_expected_revenue;
+		FP(path, N, t, n) = n;
+	}
+
+	for (int t = T-2; t >= 0; t--) {
+		
+		printf("t = %d\n", t); fflush(stdout);
+		
+		for (int n = 0; n <= N; n++) {
+			best_expected_revenue = 0.0;
+			best_index = 0;
+			for (k = 0; k <= n; k++) {
+				
+				// find sum
+				expected_revenue = 0.0;
+				for (kp = 0; kp <= k; kp++) {
+					expected_revenue += get_probability(probabilities, N, kp, k) * ((double)kp + F(optimal, N, t + 1, n - kp));
+				}
+				expected_revenue *= d[k];
+				// check if it is best; update if yes
+				if (expected_revenue > best_expected_revenue) {
+					best_expected_revenue = expected_revenue;
+					best_index = k;
+				}
+			}
+			// find the max
+			F(optimal, N, t, n) = best_expected_revenue;
+			FP(path, N, t, n) = best_index;
+			
+		}
+	}
+
+	return 0;
+}
+
--- src/algo/tim.h
+++ src/algo/tim.h
@@ -5,10 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
+#define F(optimal, N, t, n) optimal[(t)*(N+1) + (n)]
+#define FP(path, N, t, n) path[(t)*(N+1) + (n)]
+#define get_probability(probabilities, N, kp, k) probabilities[(k)*(N+1) + (kp)]
+
 
 int sample();
 extern int caller();
 void calculate_deterministic_probabilities(double* probabilities, int shares_count);
 void calculate_alpha_log_priceshifts(double* d, int shares_count, double alpha);
 int tim_resources_allocate(int N, int T, double alpha);
+int tim_optimize(int N, int T, double *probabilities, double *optimal, double *path, double *d);
+
 #endif
