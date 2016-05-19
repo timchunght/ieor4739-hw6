c4c043be0f0aa51ffa34976bc52be8eb3b14ed69
Timothy Chung <timchunght@gmail.com>
2016-05-19 02:26:35 -0400

    Making worker work with client

--- workers/worker.go
+++ workers/worker.go
@@ -7,22 +7,41 @@ package workers
 import "C"
 
 import goworkers "github.com/Scalingo/go-workers"
+import "encoding/json"
+import "fmt"
+
+// not dry
+type Message struct {
+	Shares  int
+	Periods int
+	Alpha   float64
+	Model   string
+}
 
 func TimWorker(msg *goworkers.Msg) {
 
-	fmt.Println(msg[0])
-	// _ = C.caller()
+	args := msg.Args().ToJson()
+	mArr := []Message{}
 
-	// printf("hello, tim\n");
-	// caller();
-	// int N = 1000;
-	// int T = 10;
-	// double alpha = 0.001;
-	N := 1000
-	T := 10
-	alpha := 0.001
-	modelType := "deterministic"
-	C.tim_orchestrator(C.int(N), C.int(T), C.double(alpha), C.CString(modelType))
+	err := json.Unmarshal([]byte(args), &mArr)
+	if err != nil {
+		fmt.Println("unmarshal error")
+	} else {
+		m := mArr[0]
+		fmt.Println(m)
+		// _ = C.caller()
+
+		// printf("hello, tim\n");
+		// caller();
+		// int N = 1000;
+		// int T = 10;
+		// double alpha = 0.001;
+		N := 1000
+		T := 10
+		alpha := 0.001
+		modelType := "deterministic"
+		C.tim_orchestrator(C.int(N), C.int(T), C.double(alpha), C.CString(modelType))
+	}
 
 }
 
